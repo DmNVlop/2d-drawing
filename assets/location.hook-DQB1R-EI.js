d7a7165235dcff7dfca2df164575c450f7c1d2ec
Damian Vidal <dmnvlop@gmail.com>
2024-04-24 02:30:23 +0200

    Corregido ruteo 2d-drawing

--- assets/location.hook-DQB1R-EI.js
+++
@@ -1,1 +0,0 @@
-import{e as r}from"./index-T6wk3FOQ.js";function c(a){const o=r();return new URLSearchParams(o.search).get(a)}export{c as u};
---
+++ assets/location.hook-vpIBqS-S.js
@@ -0,0 +1,1 @@
+import{e as r}from"./index-DgTlxUx8.js";function c(a){const o=r();return new URLSearchParams(o.search).get(a)}export{c as u};
